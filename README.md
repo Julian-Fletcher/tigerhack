1ac782e0a0ec0692e27867f91b4d2135c02d7bef
Julian Fletcher <37055413+Julian-Fletcher@users.noreply.github.com>
2022-10-31 16:34:27 -0500

    I did things
    Things were did

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tigerhack
-Peyton Kirby and Julian Fletcher take on TigerHacks 2022
+All the code within this project has been written in its entirety by Julian Fletcher and Peyton Kirby. 
+
+We're two freshmen CS students at the University of Missouri in the C language. All hail JimR our lord and savior. 
--- test.c
+++ test.c
@@ -1,11 +1,41 @@
 #include <stdio.h>
- 
+#include <stdlib.h>
+#define SIZE 100
+
+
+void makeArray(int * array);
+void printArray(int *array);
+void DoFancyStuff(int *array);
 
 int main(void){
     printf("HI!!!!\n");
 
-    for (int x = 0; x <100000; x++){
-        printf("X = %d\n", x);
-    }
+    int arraySize = SIZE;
+    int array[SIZE];
+    
+    makeArray(array);
+    //printArray(array);
+
 
 }
+
+void makeArray(int *array){
+    int seq;
+
+    printf("Enter a number: ");
+    scanf("%d", &seq);
+
+    for (int i = 0; i < SIZE; i++){
+        int val = rand();
+        array[i] = val;
+        printf("Array[%d] = %d\n", i, array + i);
+        
+    }
+}
+/*
+void printArray(int *array){
+    for (int i = 0; i < SIZE; i++){
+        printf("Array[%d] = %d\n", i, array + i);
+    }
+}
+*/
